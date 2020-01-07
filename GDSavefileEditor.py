7815144a7336738f26d00cf71086eacb08ec7a2d
WEGFan <goofans@qq.com>
2020-01-07 15:14:26 +0800

    rewrite

---
+++ GDSavefileEditor.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import base64
+import os
+import struct
+import traceback
+import xml
+import zlib
+from xml.dom import minidom
+
+SAVE_FILE_NAME = ['CCGameManager.dat', 'CCLocalLevels.dat']
+SAVE_FILE_PATH = os.path.join(os.getenv('LocalAppData'), 'GeometryDash')
+
+prettify_xml = False
+
+
+def print_menu() -> None:
+    os.system('cls')
+    print('Geometry Dash Savefile Encrypter & Decrypter\n'
+          '\n'
+          'Decryption code was download from https://pastebin.com/JakxXUVG by Absolute Gamer\n'
+          '\n'
+          '1. Encrypt\n'
+          '2. Decrypt\n'
+          '3. Open save file folder\n'
+          f'4. Toggle prettify XML after decrypt [Current: {"ON" if prettify_xml else "OFF"}]\n')
+
+
+def xor_bytes(data: bytes, value: int) -> bytes:
+    return bytes(map(lambda x: x ^ value, data))
+
+
+if __name__ == '__main__':
+    print_menu()
+
+    while True:
+        s = input('>>> ')
+        print()
+
+        try:
+            index = int(s)
+        except ValueError as err:
+            exit()
+
+        if index == 1:  # encrypt
+            for save_file in SAVE_FILE_NAME:
+                try:
+                    print(f'Encrypting {save_file}.xml...')
+
+                    with open(f'{save_file}.xml', 'rb') as f:
+                        decrypted_data = f.read()
+
+                    compressed_data = zlib.compress(decrypted_data)
+                    data_crc32 = zlib.crc32(decrypted_data)
+                    data_size = len(decrypted_data)
+
+                    compressed_data = b'\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x0b' + compressed_data[2:-4] + struct.pack('I I', data_crc32, data_size)
+                    encoded_data = base64.b64encode(compressed_data, altchars=b'-_')
+                    encrypted_data = xor_bytes(encoded_data, 11)
+
+                    with open(os.path.join(SAVE_FILE_PATH, save_file), 'wb') as f:
+                        f.write(encrypted_data)
+
+                    print('Done!')
+                except FileNotFoundError as err:
+                    print(f'Can\'t find {save_file}.xml in current folder!')
+                except Exception as err:
+                    print(f'Failed to encrypt {save_file}.xml!')
+                    traceback.print_exc()
+        elif index == 2:  # decrypt
+            for save_file in SAVE_FILE_NAME:
+                try:
+                    print(f'Decrypting {save_file}...')
+
+                    with open(os.path.join(SAVE_FILE_PATH, save_file), 'rb') as f:
+                        encrypted_data = f.read()
+
+                    decrypted_data = xor_bytes(encrypted_data, 11)
+                    decoded_data = base64.b64decode(decrypted_data, altchars=b'-_')
+                    decompressed_data = zlib.decompress(decoded_data[10:], -zlib.MAX_WBITS)
+
+                    if prettify_xml:
+                        try:
+                            xml_dom = minidom.parseString(decompressed_data)
+                            decompressed_data = xml_dom.toprettyxml(indent='\t', encoding='utf-8')
+                        except Exception as err:
+                            print(f'Failed to prettify {save_file}.xml! File will remain unprettified.')
+
+                    with open(f'{save_file}.xml', 'wb') as f:
+                        f.write(decompressed_data)
+
+                    print('Done!')
+                except FileNotFoundError as err:
+                    print(f'Can\'t find {save_file}.xml in save file folder!')
+                except Exception as err:
+                    print(f'Failed to decrypt {save_file}!')
+                    traceback.print_exc()
+        elif index == 3:  # open save file folder
+            os.startfile(SAVE_FILE_PATH)
+        elif index == 4:  # toggle pretty xml
+            prettify_xml = not prettify_xml
+            print_menu()
+        else:
+            exit()
+        print()
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Geometry Dash Gamesave Crypter
-Geometry Dash Gamesave encrypter &amp; decrypter
+# Geometry Dash Savefile Editor
 
-Decrypter code downloaded from https://pastebin.com/JakxXUVG by [Absolute Gamer](https://www.youtube.com/user/MrGamingGun)
+A tool that you can decrypt your *Geometry Dash* savefiles to XML document and encrypt them back.
+
+Decrypter code downloaded from <https://pastebin.com/JakxXUVG> by [Absolute Gamer](https://www.youtube.com/user/MrGamingGun)
